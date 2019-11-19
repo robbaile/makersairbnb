45e623a8b7f939fecf9475a70b5f6f0bb6e3eab6
krisn64209 <krisnorris@gmail.com>
2019-11-19 14:49:48 +0000

    completed unit tests

--- src/User.js
+++ src/User.js
@@ -24,15 +24,9 @@ class User {
   bookSpace(bookedSpace) {
     this._bookings.push(bookedSpace);
   }
-
- 
-
+  
   createSpace(createdSpace) {
     this._properties.push(createdSpace);
-  }
-
-  editPost() {
-    
   }
 
   approveBooking(space) {
